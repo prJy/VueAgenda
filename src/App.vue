31d163bf50870a24117cd2a10f00f992369627f4
prjy <deehzero@gmail.com>
2019-06-07 15:34:19 -0300

    project start

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <ul class="menu">
+      <li><a href=""></a><a href="#/">Inicio</a></li>
+      <li><a href=""></a><a href="#/login">Login</a></li>
+      <p>{{usuario.nome}}</p>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      usuario: {
+        nome: 'anderson',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+<b-container>
+  <b-row align-v="center" align-h="center">
+    <b-col cols="6">
+      <h2>
+        Login
+      </h2>
+      <b-jumbotron>
+          <b-form v-on:submit="handleSubmit($event)">
+            <b-form-group :label-cols="2" breakpoint="md" label="E-mail">
+              <b-form-input id="Usuario"
+              placeholder="email@email.com"
+              type="email" v-model="user.email"
+              required>
+              </b-form-input>
+            </b-form-group>
+              <b-form-group :label-cols="2" breakpoint="md" label="Senha">
+              <b-form-input id="password" type="password" v-model="user.password" required>
+              </b-form-input>
+          </b-form-group>
+          <b-button align-h="end" type="submit" variant="primary">Acessar</b-button>
+        </b-form>
+
+      </b-jumbotron>
+    </b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  title: 'Login',
+  data() {
+    return {
+      user: {
+        email: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    handleSubmit: (event) => {
+      event.preventDefault();
+    },
+  },
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
